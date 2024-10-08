fc76ed3af140bb0ec72ebab093f7187d6115832a
niti-di1977 <niti.dwivedi@hotmail.com>
2024-10-08 09:32:00 +0530

    New file added

---
+++ GooglePay.java
@@ -0,0 +1,48 @@
+package com.example.functionalexample;
+
+
+//MAM -- Multiple Abstract Methods -- Normal Interface 
+//SAM -- Single Abstract Methods -- functional Interface
+
+@FunctionalInterface
+public interface GooglePay {
+	
+	public int pay(int amount1 , int amount2);
+	
+     //Somebody wants to add some more functionality to this interface as per the requirement
+	
+	default void details()
+	{
+		
+		//implementation
+	}
+	
+	default void details1()
+	{
+		
+		//implementation
+	}
+	
+	static void transaction()
+	{
+		
+		
+	}
+}
+
+class Vendors
+{
+	public static void main(String[] args)
+	{
+	GooglePay gp = (a,b) -> // would not be able to create an object of interface  . we use lambda expression for that
+	{
+		
+		return a+b;
+	};
+	
+	System.out.println("Amazon Cost " + gp.pay(34,56));
+	System.out.println("Swiggy Cost " + gp.pay(34,56));
+	System.out.println("Recharge Cost " + gp.pay(34,56));
+
+}
+}
